f3efd5c2087b82777762383a242d035c1dbce9ce
denvernine <denternine99@gmail.com>
2021-04-14 09:04:38 +0000

    use volte + setup sveltekit

--- src/apps/Domains/ValueObjects/Identifier.ts
+++
@@ -1,3 +0,0 @@
-export declare interface Identifier {
-  equals(identifier: Identifier): boolean;
-}
--- src/apps/Domains/ValueObjects/StringIdentifier.ts
+++
@@ -1,5 +0,0 @@
-import {Identifier} from './Domains/ValueObjects/Identifier';
-
-export declare interface StringIdentifier extends Identifier {
-  readonly code: string;
-}
